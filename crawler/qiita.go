5a228459fe6274e7a2548860af8e18162e17517a
Masahiko Sato <sato_m@mediaxis.jp>
2020-04-21 23:14:55 +0900

    Qiitaのサムネイルを取得する処理を追加

--- crawler/qiita.go
+++ crawler/qiita.go
@@ -3,7 +3,9 @@ package crawler
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"time"
@@ -79,7 +81,11 @@ func (a *ArticleCrawler) Run() ([]qiitaResult, error) {
 				return results, err
 			}
 
-			popularItems := extractPopularItems(items)
+			var popularItems []qiitaItem
+			for _, item := range extractPopularItems(items) {
+				crawlThumbnail(tag, &item)
+				popularItems = append(popularItems, item)
+			}
 			results = append(results, qiitaResult{Tag: tag, Items: popularItems})
 			return results, nil
 		}()
@@ -99,15 +105,45 @@ func extractPopularItems(source []qiitaItem) []qiitaItem {
 	return articles
 }
 
+func crawlThumbnail(tag string, item *qiitaItem)  {
+	res, err := http.Get(item.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			panic(err.Error())
+		}
+	}()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	thumbnail, _ := doc.Find("meta[property='og:image']").First().Attr("content")
+	fmt.Println(thumbnail)
+	item.Thumbnail = thumbnail
+}
+
 type qiitaItem struct {
 	ID    string `json:"id"`
 	Title string `json:"title"`
 	URL   string `json:"url"`
 	Likes int    `json:"likes_count"`
+	Thumbnail string
 }
 
+
 func (a qiitaItem) String() string {
-	return fmt.Sprintf("id: %v, title: %v, url: %v, likes: %v", a.ID, a.Title, a.URL, a.Likes)
+	return fmt.Sprintf("id: %v, title: %v, url: %v, likes: %v, thumbnail: %v", a.ID, a.Title, a.URL, a.Likes, a.Thumbnail)
 }
 
 type qiitaResult struct {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/c8112002/news-crawler
 go 1.13
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/joho/godotenv v1.3.0
 	github.com/spf13/viper v1.6.3
