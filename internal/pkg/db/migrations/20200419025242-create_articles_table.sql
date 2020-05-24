3416636cd6a90f8eb54322507d812a890deeb9f7
sato <sato_m@mediaxis.jp>
2020-05-24 21:34:41 +0900

    articlesテーブルのカラム名を修正

--- internal/pkg/db/migrations/20200419025242-create_articles_table.sql
+++ internal/pkg/db/migrations/20200419025242-create_articles_table.sql
@@ -6,14 +6,14 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `url` VARCHAR(255) NOT NULL,
   `image` TEXT NULL,
   `crawled_at` TIMESTAMP NOT NULL,
-  `sites_id` INT UNSIGNED NOT NULL,
+  `site_id` INT UNSIGNED NOT NULL,
   `original_id` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_articles_sites_idx` (`sites_id` ASC) VISIBLE,
+  INDEX `fk_articles_site_idx` (`site_id` ASC) VISIBLE,
   INDEX `crawled_at_idx` (`crawled_at` ASC) VISIBLE,
-  UNIQUE INDEX `sites_id_original_id_unique` (`sites_id` ASC, `original_id` ASC) VISIBLE,
+  UNIQUE INDEX `site_id_original_id_unique` (`site_id` ASC, `original_id` ASC) VISIBLE,
   CONSTRAINT `fk_articles_sites`
-    FOREIGN KEY (`sites_id`)
+    FOREIGN KEY (`site_id`)
     REFERENCES `sites` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
